58eaf83a29386a5a36d0c4b14355677775fe0355
Priyanshu Raj <pr590315@gmail.com>
2026-08-13 13:32:01 +0530

    Update cpu.py

---
+++ stats/cpu.py
@@ -0,0 +1,16 @@
+import psutil
+
+class CPU():
+    def __init__(self):
+        self.core = psutil.cpu_count()
+        self.p_core = psutil.cpu_count(logical=False)
+        self.feq_cpu = psutil.cpu_freq()
+        self.disk = psutil.disk_usage('/')
+        self.ram = psutil.virtual_memory()
+
+cpu = CPU()
+print(cpu.core)
+print(cpu.p_core)
+print(cpu.feq_cpu)
+print(cpu.disk)
+print(cpu.ram)
