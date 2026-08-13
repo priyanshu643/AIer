126cf9bd55815da5d886c0eec6921e864ff625e3
Priyanshu Raj <pr590315@gmail.com>
2026-08-13 13:59:36 +0530

    all the cpu data pulling module is completed

--- stats/cpu.py
+++ stats/cpu.py
@@ -1,16 +1,21 @@
 import psutil
-
+import cpuinfo
 class CPU():
     def __init__(self):
+        self.cpu = cpuinfo.get_cpu_info()
         self.core = psutil.cpu_count()
         self.p_core = psutil.cpu_count(logical=False)
-        self.feq_cpu = psutil.cpu_freq()
+        raw_feq = psutil.cpu_freq()
+        self.max_ghz = round(raw_feq.max / 1000, 2)
+        self.min_ghz = round(raw_feq.min / 1000, 2)
         self.disk = psutil.disk_usage('/')
         self.ram = psutil.virtual_memory()
 
-# cpu = CPU()
-# print(cpu.core)
-# print(cpu.p_core)
-# print(cpu.feq_cpu)
-# print(cpu.disk)
-# print(cpu.ram)
+cpu = CPU()
+print(cpu.cpu['brand_raw'])
+print(cpu.core)
+print(cpu.p_core)
+print(cpu.max_ghz)
+print(cpu.min_ghz)
+print(cpu.disk)
+print(cpu.ram)
